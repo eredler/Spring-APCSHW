0daed040ec9498e08a20d9543f4c71f6cf1c56d5
Emily Redler <esredler@gmail.com>
2015-03-09 13:19:36 -0400

    started quickSort

--- 06QuickSelect/Sorts.java
+++ 06QuickSelect/Sorts.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
-public class QuickSelect {
-    int[] ary;
-    int len;
+public class Sorts {
+    public static int[] ary;
+    public int len;
 
     public static Random r = new Random();
 
-    public QuickSelect(int[] a, int l){
+    public Sorts(int[] a, int l){
 	ary = a;
 	len = l;
     }
@@ -62,32 +62,32 @@ public class QuickSelect {
 
     //
     //
-    //doesn't work yet...
-    public void inPlace(int si, int ei){
+    //works but returns n+1th value if start counting from 0
+    public static int inPlace(int n, int si, int ei){
 	int li = si;
 	int ri = ei-1;
 	int hold;
 	int pivot = ary[r.nextInt(ei-si) + si];
 	System.out.println(Arrays.toString(ary));
-	System.out.println("pivot: " + pivot);
+	//	System.out.println("pivot: " + pivot);
 	while (li < ri){
-	    System.out.println("li: " + li + "  ri: " + ri);
+	    //	    System.out.println("li: " + li + "  ri: " + ri);
 	    if (ary[li] < pivot){
 		hold = ary[li];
-		System.out.println("looking at " + ary[li]);
-		System.out.println("li hold: " + hold);
+		//	System.out.println("looking at " + ary[li]);
+		//	System.out.println("li hold: " + hold);
 		// ary[li] = ary[];
 		// ary[li] = hold;
 		li++;
-		System.out.println(Arrays.toString(ary));
+		//		System.out.println(Arrays.toString(ary));
 	    } else if (ary[li] > pivot){
 		hold = ary[ri];
-		System.out.println("looking at " + ary[li]);
-		System.out.println("ri hold: " + hold);
+		//	System.out.println("looking at " + ary[li]);
+		//	System.out.println("ri hold: " + hold);
 		ary[ri] = ary[li];
 		ary[li] = hold;
 		ri--;
-		System.out.println(Arrays.toString(ary));		
+		//		System.out.println(Arrays.toString(ary));		
 	    } else {
 		hold = ary[ri];
 		ary[ri] = ary[li];
@@ -96,8 +96,17 @@ public class QuickSelect {
 		
 	    }
 	}
+
+	if (li == n-1){
+	    return ary[li];
+	}
+	else if (li < n-1){
+	    return inPlace(n, li, ei);
+	}else{
+	    return inPlace(n, si, li);
+	}
 	//	ary[ri] = pivot;
-	System.out.println(Arrays.toString(ary));
+	//System.out.println(Arrays.toString(ary));
     }
 
 
@@ -114,9 +123,37 @@ public class QuickSelect {
 	return Arrays.toString(ary);
     }
 
+    //
     //weird with some cases (0,10) and gives n-1th place (if start counting from 0)
-    public int select(int n){
+    //select using partition
+    //
+    public int selectP(int n){
 	return partition(n, 0, ary.length - 1);
+    }
+
+    //also weird with some cases
+    //select using inPlace
+    //
+    public static int selectIP(int n){
+	return inPlace(n, 0, ary.length - 1);
+    }
+
+    // doesn't compile
+    //
+    // should only have 1 argument: int[]    
+    public static void quickSort(int[] a, int li, int ei){
+	int where = a.length/2;
+	int x = a[where];
+	if (a.length/2 >= 1){	
+	    // (ei-li)/2
+	    //           |
+	    //           |  selectIP() returns an int, argument supposed to be int[]
+	    //           V
+	    quickSort(selectIP(l.length/2), li, where);
+	    quickSort(selectIP(l.length/2), where, ei);
+	} else {
+	    //   return a;
+	}
     }
 
     public static void main(String[]args){
@@ -132,11 +169,12 @@ public class QuickSelect {
 	    }
 	}
 
-	QuickSelect qs = new QuickSelect(a,10);
+	Sorts qs = new Sorts(a,10);
 
 	System.out.println(qs.toString());
 	//	partition(a,0,l);
-	qs.inPlace(0,l);
+	System.out.println(quickSort(a));
+	//	System.out.println(qs.selectP(5));
 	//	System.out.println(qs.select(5));
 	//System.out.println(qs.select(0));
 	//	System.out.println(qs.select(6));
