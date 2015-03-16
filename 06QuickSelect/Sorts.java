156225e67a407c43b640e285181f783bacf9dd02
Emily Redler <esredler@gmail.com>
2015-03-16 17:49:58 -0400

    fixed quick sort, doesn't work with duplicates yet

--- 06QuickSelect/Sorts.java
+++ 06QuickSelect/Sorts.java
@@ -11,106 +11,69 @@ public class Sorts {
 	len = l;
     }
 
+    public String name(){
+	return "redler.emily";
+    }
+
     public int getLen(){
 	return len;
     }
 
-    /* public int partition(int n, int si, int ei){
-	int[] d = new int[ary.length];
+    public static int inPlace(int si, int ei){
+
 	int li = si;
-	int ri = ei-1;
-	
-	for (int i = 0; i < si; i++){
-	    d[i] = ary[i];
-	}
-	for (int c = ei; c < ary.length; c++){
-	    d[c] = ary[c];
-	}
-	//	System.out.println(Arrays.toString(d));
-	int pivot = ary[r.nextInt(ei-si) + si];
-	//	System.out.println(pivot);
-	
-	for (int x = si; x < ei; x++){
-	    if (ary[x] == pivot){
-	    } else if (ary[x] < pivot){
-		d[li] = ary[x];
-		//		System.out.println("d[li] " + d[li] + " li: " + li);
-		li++;
-		//		System.out.println(Arrays.toString(d));
-	    } else {
-		d[ri] = ary[x];
-		//	System.out.println("d[ri]: " + d[ri] + " ri: " + ri);
-		//	System.out.println(Arrays.toString(d));
-		ri--;
-	    }
-	}
-	
-	d[ri] = pivot;
-	System.out.println(Arrays.toString(d));
-
-	ary = d;
-
-	if (li == n-1){
-	    return d[li];
-	}
-	else if (li < n-1){
-	    return partition(n, li, ei);
-	}else{
-	    return partition(n, si, li);
-	}
-	}*/
-
-    //
-    //
-    //works with all cases except n = a.length-1
-    //
-    //when smallest number is chosen as pivot, doesn't work
-    //
-    public static int[] inPlace(int si, int ei){
-	int li = si;
-	int ri = ei-1;
+	int ri = ei;
 	int hold;
-	int index = r.nextInt(ei-si) + si;	
+	int index = r.nextInt(1 - si + ei) + si;	
 	int pivot = ary[index];
-	System.out.println(Arrays.toString(ary));
-	System.out.println("pivot: " + pivot);
-	while (li < ri){
-	    //	    System.out.println("li: " + li + "  ri: " + ri);
+	//	System.out.println(Arrays.toString(ary));
+	//	System.out.println("pivot: " + pivot);
+
+	while (li <= ri && si != ei){
+
 	    if (ary[li] < pivot){
-		System.out.println(ary[li] + " < " + pivot);
-		//	System.out.println("li hold: " + hold);
-		// ary[li] = ary[];
-		// ary[li] = hold;
+		//	System.out.println(ary[li] + " < " + pivot);
+
+		hold = ary[li];
+		ary[li] = ary[si];
+		ary[si] = hold;
+		si++;
+
+		if (hold == pivot){
+		    index = si;
+		} else if (ary[li] == pivot){
+		    index = li;
+		}
+
 		li++;
-		System.out.println(Arrays.toString(ary));
+		//		System.out.println(Arrays.toString(ary));
+
 	    } else if (ary[li] > pivot){
-		hold = ary[ri];
-		System.out.println(ary[li] + " > " + pivot);
-		//System.out.println("ri hold: " + hold);
-		ary[ri] = ary[li];
-		ary[li] = hold;
-		ri--;
-		System.out.println(Arrays.toString(ary));		
+		hold = ary[li];
+		//	System.out.println(ary[li] + " > " + pivot);
+		ary[li] = ary[ei];
+		ary[ei] = hold;
+		ei--;
+		//		System.out.println(Arrays.toString(ary));
+	
+		if (hold == pivot){
+		    index = si;
+		} else if (ary[li] == pivot){
+		    index = li;
+		}
+
 	    } else {
-		hold = ary[ri];
-		ary[ri] = ary[li];
-		ary[li] = hold;
-		//ei--;	
+		li++;
 	    }
 	}
 
-	int[] d;
-	if (si == ri || li == ei){
-	    System.out.println("si: " + si + " ri: " + ri + " li: " + li + " ei: " + ei);
-	    System.out.println("yo");
-	    return ary;
-	}
-	d = inPlace(si, ri);
-	d = inPlace(li, ei);
-	return d;
+    hold = ary[si];
+    ary[si] = pivot;
+    ary[index] = hold;
+    return index;
     }
 
-
+    //for making list with no duplicates for testing
     public static boolean in(int[] a, int e){
 	for (int i = 0; i < a.length; i++){
 	    if (a[i] == e){
@@ -124,28 +87,17 @@ public class Sorts {
 	return Arrays.toString(ary);
     }
 
-    //
-    //weird with some cases (eg len of list -1)
-    //
-    //select using partition
-    //
-    /*
-    public int selectP(int n){
-	return partition(n, 0, ary.length - 1);
-    }
-    */
-    //also weird with some cases
-    //
-    //select using inPlace
-    //
-    public static int[] selectIP(int n){
-	return inPlace(0, ary.length - 1);
+    //doesn't deal with duplicates
+    public static void quickSort(){
+	quickSort(0,ary.length-1);
     }
 
-    
-    // doesn't always sort correctly  
-    public static int[] quickSort(int[] a){
-	return inPlace(0,a.length);
+    public static void quickSort(int si, int ei){
+	if (si < ei){
+	    int index = inPlace(si, ei);
+	    quickSort(si, index-1);
+	    quickSort(index+1, ei);
+	}
     }
     
     public static void main(String[]args){
@@ -153,22 +105,23 @@ public class Sorts {
 	int[] a = new int[l];
 
 	for (int i = 0; i < l; i++){
-	    int n = r.nextInt(100)+1;
-	    if (!in(a,n)){
+	    int n = r.nextInt(10)+1;
+	    //    if (!in(a,n)){
 		a[i] = n;
-	    } else {
-		i--;
-	    }
+		//    } else {
+		//		i--;
+		
+		//	    }
 	}
-
-	for (int ii = 0; ii <= 9; ii++) {
-	    a[ii] = 9-ii;
-	}
+	//	for (int ii = 0; ii <= 9; ii++) {
+	//	    a[ii] = 9-ii;
+	//	}
 
 	Sorts qs = new Sorts(a,l);
 
 	System.out.println(qs.toString());
-	System.out.println(Arrays.toString(quickSort(a)));
+	qs.quickSort();
+	System.out.println(qs.toString());
     }
 
 
