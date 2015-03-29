7c21c1cdef848cb17e51dd472b57c9a74138faba
Emily Redler <esredler@gmail.com>
2015-03-29 18:11:25 -0400

    added toString, fixed other methods, added shrink
    shrink doesn’t work

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -5,19 +5,25 @@ public class MyDeque<T> {
     public int size;
     public int head;
     public int tail;
+    public boolean shrinkOn = false;
 
     public MyDeque(){
-	d = new Object[10];
+	d = new Object[5];
 	size = 0;
 	head = 0;
 	tail = 0;
     }
 
+    public String toString(){
+	return Arrays.toString(d);
+    }
+
     public void addFirst(T value){
 	// when array is full, resize, then add
-	if (size >= d.length-1){
-	    resizeCopy();
+	if (size+1 > d.length){
+	    grow();
 	}
+	
 	if (d[head] != null){
 	    if (head == 0){
 		head = d.length-1;
@@ -32,8 +38,8 @@ public class MyDeque<T> {
 
     public void addLast(T value){
 	// when array is full, resize, then add
-	if (size >= d.length-1){
-	    resizeCopy();
+	if (size+1 > d.length){
+	    grow();
 	}
 	if (tail >= d.length){
 	    tail = 0;
@@ -46,14 +52,18 @@ public class MyDeque<T> {
 
     public T removeFirst(){
 	// throws NoSuchElementException
-	try {
+	try {	    
+	    if ((size+1 <= d.length/4) && shrinkOn){
+		shrink();
+	    }
 	    T hold = (T)d[head];
 	    d[head] = null;
-	    if (head == d.length){
+	    if (head >= d.length-1){
 		head = 0;
 	    } else {
 		head++;
 	    }
+	    size--;
 	    return hold;
 	} catch (NoSuchElementException e){
 	    throw new NoSuchElementException();
@@ -63,6 +73,9 @@ public class MyDeque<T> {
     public T removeLast(){
 	// throws NoSuchElementException
 	try {
+	    if ((size+1 <= d.length/4) && shrinkOn){
+		shrink();
+	    }
 	    T hold = (T)d[tail];
 	    d[tail] = null;
 	    if (tail == 0){
@@ -70,6 +83,7 @@ public class MyDeque<T> {
 	    } else {
 		tail--;
 	    }
+	    size--;
 	    return hold;
 	} catch (NoSuchElementException e){
 	    throw new NoSuchElementException();
@@ -95,27 +109,57 @@ public class MyDeque<T> {
 	}
     }
 
-    public void resizeCopy(){
+    public void grow(){
 	// resize array (double) and copy to new array
 	Object[] ans = new Object[d.length*2];
 	int x = 0;
-	for (int i = head; i < d.length || i < tail; i++){
+	for (int i = head; i < d.length || i <= tail; i++){
 	    ans[x] = d[i];
 	    x++;
 	} 
 	if (tail < head){
-	    for (int i = tail; i < head; i++){
+	    for (int i = 0; i <= tail; i++){
 		ans[x] = d[i];
 		x++;
 	    }
 	}
 	d = ans;
 	head = 0;
-	tail = size;
+	tail = size-1;
     }
 
-    public static void main(String[]args){
+    public void shrink(){
+	// resize array (half) and copy to new array
+	Object[] ans = new Object[d.length/2];
+	int x = 0;
+	for (int i = head; i < d.length || i <= tail; i++){
+	    ans[x] = d[i];
+	    x++;
+	} 
+	if (tail < head){
+	    for (int i = 0; i <= tail; i++){
+		ans[x] = d[i];
+		x++;
+	    }
+	}
+	d = ans;
+	head = 0;
+	tail = size-1;
+    }
+
+    public static void main(String[]args){	
 	MyDeque<String> md = new MyDeque<String>();
+
+	try {
+	    if (args[0] == "t"){
+		md.shrinkOn = true;
+	    } else {
+		md.shrinkOn = true;
+	    }
+	} catch (ArrayIndexOutOfBoundsException e){
+
+	}
+
 
 	md.addFirst("hi");
 	System.out.println(md.getFirst()); // hi
@@ -131,6 +175,7 @@ public class MyDeque<T> {
 	System.out.println(md.getFirst()); // ice cream
 	System.out.println(md.removeFirst()); // ice cream
 	System.out.println(md.getFirst()); // emily
+	System.out.println(md.toString());
 	System.out.println(md.removeLast()); // jelly
 	System.out.println(md.getLast()); // redler
 
