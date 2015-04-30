569f5bee1b3d7e8c566ae0fed5d9cb5365ec3400
eredler <esredler@gmail.com>
2015-04-30 13:15:35 -0400

    started BSTree

---
+++ 12TreeLab/BSTreeNode.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class BSTreeNode<T extends Comparable> {
+
+    private T data;
+    private BSTreeNode<T> left;
+    private BSTreeNode<T> right;
+
+    private int numOf;
+
+    public BSTreeNode( T d ) {
+ 
+	data = d;
+	left = right = null;
+	setNumOf(1);
+
+    }
+    
+    //accessors
+    public T getData() {
+	return data;
+    }
+    public BSTreeNode<T> getLeft() {
+	return left;
+    }
+    public BSTreeNode<T> getRight() {
+	return right;
+    }
+    public int getNumOf(){
+	return numOf;
+    }
+
+    //mutators
+    public void setData( T d ) {
+	data = d;
+    }
+    public void setLeft( BSTreeNode<T> l ) {
+	left = l;
+    }
+    public void setRight( BSTreeNode<T> r ) {
+	right = r;
+    }
+    public void setNumOf(int n){
+	numOf = n;
+    }
+}
