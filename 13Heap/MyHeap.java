b3f21dde25b0e3797eca4deb71c8e30c9a605e64
Emily Redler <esredler@gmail.com>
2015-05-13 20:34:32 -0400

    it works. i did it wrong, but it works (i think).

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -7,15 +7,20 @@ public class MyHeap {
     final static boolean MAX_HEAP = true;
     final static boolean MIN_HEAP = false;
 
-    public boolean isMax;
+    public boolean mode;
+
+    public Integer root;
 
     public MyHeap() {
-	isMax = MAX_HEAP;
+	mode = MAX_HEAP;
 	heap = new ArrayList<Integer>();
+	root = null;
     }
 
-    public MyHeap(boolean _isMax){
-	isMax = _isMax;
+    public MyHeap(boolean m){
+	mode = m;
+	heap = new ArrayList<Integer>();
+	root = null;
     }
 
     public String toString(){
@@ -23,16 +28,49 @@ public class MyHeap {
     }
     
 
-    public int remove(){
-
+    public Integer remove(){
+	Integer hold = root;
+	heap.remove(root);
+	root = heap.get(0);
+	return hold;
     }
 
-    public void add(int v){
-
+    public void add(Integer v){
+	if (root == null){
+	    root = v;
+	} else if (mode == MAX_HEAP){
+	    if (v >= root){
+		heap.add(0,v);
+		root = v;
+	    } else {
+		for (int i = 0; i < heap.size(); i++){
+		    if (heap.get(i) < v){
+			heap.add(i,v);
+			return;
+		    }
+		    heap.add(heap.size(),v);
+		    return;
+		}
+	    }
+	} else {
+	    if (v <= root){
+		heap.add(0,v);
+		root = v;
+	    } else {
+		for (int i = 0; i < heap.size(); i++){
+		    if (heap.get(i) > v){
+			heap.add(i,v);
+			return;
+		    }
+		    heap.add(heap.size(),v);
+		    return;
+		}
+	    }
+	}
     }
 
     public int peek(){
-
+	return root;
     }
 
 }
